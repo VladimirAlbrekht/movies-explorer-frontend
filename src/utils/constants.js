9429599697de368fe613f9731253c975e346796b
Vladimir Albrekht <97494847+VladimirAlbrekht@users.noreply.github.com>
2023-06-25 17:57:58 +0800

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://api.my-movies.nomoredomains.rocks';
+const BASE_URL = 'https://api.my-movies.nomoredomains.rocks';
 const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 
 const SUCCESSFUL_CODE = 200;
