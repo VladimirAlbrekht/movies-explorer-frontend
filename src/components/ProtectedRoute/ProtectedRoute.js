69e660ac20958fc4f2d80acc62e3815a15d47247
Vladimir Albrekht <97494847+VladimirAlbrekht@users.noreply.github.com>
2023-06-26 16:41:42 +0800

    Update ProtectedRoute.js

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -5,7 +5,7 @@ const ProtectedRoute = ({ element: Component, ...props }) => {
   return props.loggedIn ? (
     <Component {...props} />
   ) : (
-    <Navigate to="/signin" replace />
+    <Navigate to="/" replace />
   );
 };
 
